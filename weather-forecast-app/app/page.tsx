d8f1d10d80414ad5cd74277565839a4c60df16b7
Sharon <156696215+Biwott362@users.noreply.github.com>
2025-04-23 14:34:12 +0300

    File renamed

--- weather-forecast-app/app/page.tsx
+++ weather-forecast-app/app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
-
-import Input from "./component/input";
+import Input from "./components/Input";
 
 const Home = () => {
      return (
      <div className="bg-cover bg-gradient-to-r from-blue-100 to-blue-300 h-screen">
       <div className ="bg-white/25 w-full rounded-ig flex flex-col h-full" >
       {/* INPUT AND LOGO */}
-        <div className="flex flex-col items-center justify-center p-12 h-full">
+        <div className="flex flex-col items-center justify-center h-full">
           <input />
           <h1 className="text-3xl font-bold text-center">Weather Forecast App</h1>
         </div>
