c6d55bc53f9174627f534d7119894cba759d9a35
Sharon <156696215+Biwott362@users.noreply.github.com>
2025-04-23 20:02:31 +0300

    Changed color to orange

--- weather-forecast-app/app/page.tsx
+++ weather-forecast-app/app/page.tsx
@@ -29,12 +29,12 @@ const Home = () => {
   }
 };
      return (
-     <div className="bg-cover bg-gradient-to-r from-blue-100 to-blue-300 h-screen">
+     <div className="bg-cover bg-gradient-to-r from-orange-100 to-orange-300 h-screen">
       <div className ="bg-white/25 w-full rounded-ig flex flex-col h-full" >
       {/* INPUT AND LOGO */}
           {/* Logo */}
         <div className="flex justify-center items-center pt-4">
-          <h1 className="absolute top-4 left-6 text-3xl font-bold bg-gradient-to-r from-blue-400 to-blue-300 bg-clip-text text-transparent">Weather Forecast</h1>
+          <h1 className="absolute top-4 left-6 text-3xl font-bold bg-gradient-to-r from-orange-400 to-orange-300 bg-clip-text text-transparent">Weather Forecast</h1>
         </div>
            {/* Input Section */}
           <Input handleSearch={handleSearch} 
