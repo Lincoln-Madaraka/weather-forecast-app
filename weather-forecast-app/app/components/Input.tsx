41c509f5f09b9867ce4f04ebd00a7bdde857956b
Sharon <156696215+Biwott362@users.noreply.github.com>
2025-04-23 17:07:12 +0300

    Updated Logo and input

--- weather-forecast-app/app/components/Input.tsx
+++ weather-forecast-app/app/components/Input.tsx
@@ -4,13 +4,13 @@ import { FiSearch } from "react-icons/fi";
 const Input = () => {
   return (
     <div className="flex justify-center items-center h-screen"> {/* Centering the form */}
-      <form className="flex items-center bg-white p-4 rounded-lg shadow-lg w-3/4 md:w-2/4"> {/* Form Container */}
+      <form className="flex items-center bg-white p-2 rounded-lg shadow-lg w-3/4 md:w-2/4"> {/* Form Container */}
         <input
           type="text"
           placeholder="Enter city name"
           className="w-full p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-        <div className="ml-2">
+        <div className="ml-1">
           <FiSearch /> {/* Search icon */}
         </div>
       </form>
