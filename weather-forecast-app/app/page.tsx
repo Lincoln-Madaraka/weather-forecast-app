9cdcc9e2738f2a9346528483bb1965cd41159ac5
Sharon <156696215+Biwott362@users.noreply.github.com>
2025-04-23 15:34:22 +0300

    Removed padding

--- weather-forecast-app/app/page.tsx
+++ weather-forecast-app/app/page.tsx
@@ -6,9 +6,9 @@ const Home = () => {
      <div className="bg-cover bg-gradient-to-r from-blue-100 to-blue-300 h-screen">
       <div className ="bg-white/25 w-full rounded-ig flex flex-col h-full" >
       {/* INPUT AND LOGO */}
-        <div className="flex flex-col items-center justify-center h-full">
+        <div>
           <input />
-          <h1 className="text-3xl font-bold text-center">Weather Forecast App</h1>
+          <h1 className="text-3xl font-bold text-center">Weather Forecast</h1>
         </div>
       </div>
      </div>
