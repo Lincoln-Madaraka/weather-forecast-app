f558570df22ef0a6377b8cf23d9776337f7ebe88
Sharon <156696215+Biwott362@users.noreply.github.com>
2025-04-23 17:53:25 +0300

    Updated the colour of Logo

--- weather-forecast-app/app/page.tsx
+++ weather-forecast-app/app/page.tsx
@@ -8,7 +8,7 @@ const Home = () => {
       {/* INPUT AND LOGO */}
           {/* Logo */}
         <div className="flex justify-center items-center pt-4">
-          <h1 className="absolute top-4 left-6 text-3xl font-bold bg-gradient-to-r from-white to-blue-200 bg-clip-text text-transparent">Weather Forecast</h1>
+          <h1 className="absolute top-4 left-6 text-3xl font-bold bg-gradient-to-r from-blue-100 to-blue-200 bg-clip-text text-transparent">Weather Forecast</h1>
         </div>
            {/* Input Section */}
 
