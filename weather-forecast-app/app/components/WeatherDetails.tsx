f10159f6e264fe92152cd060f99bee42e31bbb37
Sharon <156696215+Biwott362@users.noreply.github.com>
2025-04-24 21:06:04 +0300

    Made background dynamic in weather details

--- weather-forecast-app/app/components/WeatherDetails.tsx
+++ weather-forecast-app/app/components/WeatherDetails.tsx
@@ -1,8 +1,32 @@
 const WeatherDetails = ({data}) => {
-  return (
-    <div className="w-full max-w-3xl mx-auto flex flex-col items-center justify-center px-6 py-6 bg-white/90 rounded-2xl shadow-xl">
-    <h2 className="text-3xl font-bold text-gray-800 drop-shadow mb-6">Weather Details</h2>
 
+  function getWeatherBg(weatherMain: string): string {
+    switch (weatherMain.toLowerCase()) {
+      case "clear":
+        return "bg-gradient-to-br from-yellow-100 via-blue-100 to-white";
+      case "clouds":
+        return "bg-gradient-to-br from-gray-100 via-slate-200 to-white";
+      case "rain":
+      case "drizzle":
+        return "bg-gradient-to-br from-blue-100 via-gray-200 to-white";
+      case "thunderstorm":
+        return "bg-gradient-to-br from-indigo-200 via-gray-300 to-white";
+      case "snow":
+        return "bg-gradient-to-br from-white via-blue-50 to-gray-100";
+      case "mist":
+      case "fog":
+        return "bg-gradient-to-br from-gray-200 via-gray-100 to-white";
+      default:
+        return "bg-gradient-to-br from-white via-sky-100 to-blue-100";
+    }
+  }
+  
+
+  return (
+    <div
+  className={`w-full max-w-3xl mx-auto flex flex-col items-center justify-center px-6 py-6 rounded-2xl shadow-xl ${getWeatherBg(data.weather[0].main)}`}
+>
+    <h2 className="text-3xl font-bold text-gray-800 drop-shadow mb-6">Weather Details</h2>
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-10 gap-y-4 w-full text-gray-800">
       <p><span className="font-semibold">Location:</span> {data.name}, {data.sys.country}</p>
       <p><span className="font-semibold">Weather:</span> {data.weather[0].description}</p>
